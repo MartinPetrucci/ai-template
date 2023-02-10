1355e93ff4fda745d3981386007a9dc0674a83e8
Martin Petrucci <martinguillermo.petrucci.contractor@gdevtools.com>
2023-02-09 23:21:56 -0300

    error message

--- src/pages/api/images.ts
+++ src/pages/api/images.ts
@@ -14,7 +14,7 @@ export default async function image(req: NextApiRequest, res: NextApiResponse) {
     console.log(response.data.data);
     res.status(200).json({ images: response.data.data });
   } catch (error) {
-    console.log("hubo un error", error);
+    console.log("hubo un error", error.message);
     res.status(500).end();
   }
 }
